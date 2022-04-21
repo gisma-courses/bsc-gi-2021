1482a85c6d3830eac19a1b61e733ce3c333dd23b
Chris Reudenbach <reudenbach@uni-marburg.de>
2022-04-21 17:37:13 +0200

    update courses

--- docs/makelist.R
+++ docs/makelist.R
@@ -1,10 +1,10 @@
 # get all related files
 files = list.files(dirname(rstudioapi::getActiveDocumentContext()$path), 
                    recursive = TRUE, full.names = TRUE)
-repo = strsplit(strsplit(files[[1]],"courses/moer")[[1]][2],"/")[[1]][2]
+repo = strsplit(strsplit(files[[1]],"gisma-courses/")[[1]][2],"/")[[1]][1]
 md_files = xfun::sans_ext(basename(files[grep("/units/", files)]))
 outpath = paste0(getwd(),"/docs/assets/pdf/")
-url = paste0("https://geomoer.github.io/",repo,"/")
+url = paste0("https://gisma-courses.github.io/",repo,"/")
 localURL = paste0("http://127.0.0.1:4000/",repo,"/")
 pan_arguments=' -f markdown-raw_tex -V colorlinks -V urlcolor=NavyBlue -V toccolor=Reds -f html-native_divs '
 wkhtml_arguments=" --page-size A4 --footer-left [webpage]  --footer-font-size 8 --footer-line --header-left [title] --header-line --margin-top 30 --margin-left 25 --margin-right 25 --margin-bottom 15 "
@@ -14,10 +14,10 @@ wkhtml_arguments=" --page-size A4 --footer-left [webpage]  --footer-font-size 8 
 # system(paste0("pandoc ",url,"index.html ",pan_arguments, "-o ",outpath, "index.pdf ","--pdf-engine=pdflatex"),wait = TRUE)
 
 res= lapply(md_files, function(x){
-  cmd=paste0(wkhtml_arguments, " ",url,strsplit(strsplit(strsplit(x = x,split = "_")[[1]][1],"/")[[1]][1],"-")[[1]][1],"/",xfun::sans_ext(basename(x)),".html ",outpath, xfun::sans_ext(basename(x)),".pdf")
+  cmd=paste0(wkhtml_arguments, " ",localURL,strsplit(strsplit(strsplit(x = x,split = "_")[[1]][1],"/")[[1]][1],"-")[[1]][1],"/",xfun::sans_ext(basename(x)),".html ",outpath, xfun::sans_ext(basename(x)),".pdf")
   system2("wkhtmltopdf",cmd,wait = TRUE)
 })
-system(paste0("wkhtmltopdf ",wkhtml_arguments, " ",url,"index.html ",outpath, "index.pdf "),wait = TRUE)
+system(paste0("wkhtmltopdf ",wkhtml_arguments, " ",localURL,"index.html ",outpath, "index.pdf "),wait = TRUE)
 
 
 
